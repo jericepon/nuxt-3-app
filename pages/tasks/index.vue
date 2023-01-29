ce06affb9abd3dfed1d8f799286b401367763c11
jericepon <jericepon@gmail.com>
2023-01-29 22:10:45 +0800

    Pages

---
+++ pages/tasks/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Tasks</h1>
+</template>
+
+<script setup>
+definePageMeta({
+  title: "Tasks",
+  description: "Tasks page",
+  keywords: "tasks, page",
+});
+</script>
+
+<style lang="scss" scoped></style>
