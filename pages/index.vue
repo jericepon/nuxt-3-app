700a564f9ba1c6cefcbfb718a2a169719706ee85
jericepon <jericepon@gmail.com>
2023-01-29 22:10:35 +0800

    Initial

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Home</h1>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, odio.</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
